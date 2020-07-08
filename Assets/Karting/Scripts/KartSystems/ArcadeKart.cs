f4fe88896743ca4b94bc46a062b9c44eed4f6f25
Cristian Montenegro <git@snowkrown.com>
2020-07-08 18:10:02 -0500

    Added lose control mechanic

--- Assets/Karting/Scripts/KartSystems/ArcadeKart.cs
+++ Assets/Karting/Scripts/KartSystems/ArcadeKart.cs
@@ -6,13 +6,24 @@ namespace KartGame.KartSystems
     [System.Serializable]
     public class ArcadeKart : MonoBehaviour
     {
+        [System.Serializable]
+        public class StatPowerUp : Powerup
+        {
+            public ArcadeKart.Stats modifiers;
+        }
+
+        [System.Serializable]
+        public class LoseControl : Powerup
+        {
+
+        }
+
         /// <summary>
         /// Contains parameters that can adjust the kart's behaviors temporarily.
         /// </summary>
         [System.Serializable]
-        public class StatPowerup
+        public class Powerup
         {
-            public ArcadeKart.Stats modifiers;
             public string PowerUpID;
             public float ElapsedTime;
             public float MaxTime;
@@ -128,7 +139,9 @@ namespace KartGame.KartSystems
 
         // can the kart move?
         bool canMove = true;
-        List<StatPowerup> activePowerupList = new List<StatPowerup>();
+        //The player has control of the kart?
+        private bool hasControl;
+        List<Powerup> activePowerupList = new List<Powerup>();
         GameObject lastGroundCollided = null;
         ArcadeKart.Stats finalStats;
 
@@ -158,15 +171,15 @@ namespace KartGame.KartSystems
             AirPercent = 1 - GroundPercent;
 
             // gather inputs
-            float accel = Input.y;
-            float turn = Input.x;
+            float accel = hasControl ? Input.y : 1;
+            float turn = hasControl ? Input.x : Random.Range(-1, 1);
 
             // apply vehicle physics
             GroundVehicle(minHeight);
+
             if (canMove)
-            {
                 MoveVehicle(accel, turn);
-            }
+
             GroundAirbourne();
 
             // animation
@@ -192,11 +205,14 @@ namespace KartGame.KartSystems
 
         void TickPowerups()
         {
+            //Set player control every tick
+            hasControl = true;
+
             // remove all elapsed powerups
             activePowerupList.RemoveAll((p) => { return p.ElapsedTime > p.MaxTime; });
 
             // zero out powerups before we add them all up
-            var powerups = new Stats();
+            var statsPoweups = new Stats();
 
             // add up all our powerups
             for (int i = 0; i < activePowerupList.Count; i++)
@@ -207,11 +223,22 @@ namespace KartGame.KartSystems
                 p.ElapsedTime += Time.deltaTime;
 
                 // add up the powerups
-                powerups += p.modifiers;
+                if (p is StatPowerUp)
+                    statsPoweups += (p as StatPowerUp).modifiers;
+
+                //Disable movement if player no has control
+                if (p is LoseControl)
+                    hasControl = false;
+            }
+
+            if (!hasControl)
+            {
+                statsPoweups.TopSpeed = 5;
+                statsPoweups.Steer = baseStats.Steer * 2;
             }
 
             // add powerups to our final stats
-            finalStats = baseStats + powerups;
+            finalStats = baseStats + statsPoweups;
 
             // clamp values in finalstats
             finalStats.Grip = Mathf.Clamp(finalStats.Grip, 0, 1);
@@ -456,7 +483,7 @@ namespace KartGame.KartSystems
             }
         }
 
-        public void AddPowerup(StatPowerup statPowerup)
+        public void AddPowerup(Powerup statPowerup)
         {
             activePowerupList.Add(statPowerup);
         }
